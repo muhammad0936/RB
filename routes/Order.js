fb4d0fc5343a86a637e69f4f38d7f56d9fa00236
Muhammad khattab <muhammadkh.15.9.2000@gmail.com>
2025-02-11 18:08:29 +0300

    move isAuth middleware in order router to each request

--- routes/Order.js
+++ routes/Order.js
@@ -4,24 +4,21 @@ const orderController = require('../controllers/Payment');
 const isAuth = require('../middlewares/isAuth');
 const multerGlobal = require('../middlewares/multerGlobal');
 
-// Add authentication middleware to protect routes
-router.use(isAuth);
-
 // Checkout route
-router.get('/checkout', multerGlobal, orderController.checkout);
+router.get('/checkout', multerGlobal, isAuth, orderController.checkout);
 
 // Payment verification route
 // router.post('/verify-payment', multerGlobal, orderController.verifyPayment);
 
 // Temporary test endpoints (remove in production)
-router.get('/payment-callback', multerGlobal, (req, res) => {
+router.get('/payment-callback', multerGlobal, isAuth, (req, res) => {
   res.status(200).json({
     success: true,
     data: req.query,
   });
 });
 
-router.get('/payment-error', multerGlobal, (req, res) => {
+router.get('/payment-error', multerGlobal, isAuth, (req, res) => {
   res.status(400).json({
     success: false,
     error: req.query,
