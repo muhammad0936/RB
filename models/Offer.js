de1bf7b28226dd28b22bc2eeb2dc2de0729f5d50
Muhammad khattab <muhammadkh.15.9.2000@gmail.com>
2025-02-28 02:10:22 +0300

    change image to logo in product schema

---
+++ models/Offer.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const Schema = mongoose.Schema;
+const offerSchema = new Schema(
+  {
+    description: String,
+    products: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        newPrice: {
+          type: Number,
+          required: true,
+        },
+        expirationDate: {
+          type: Date,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+offerSchema.plugin(mongoosePaginate);
+module.exports = mongoose.model('Offer', offerSchema);
--- models/Product.js
+++ models/Product.js
@@ -21,7 +21,7 @@ const productSchema = new Schema(
         required: true,
       },
     ],
-    image: {
+    logo: {
       url: String, // Cloudinary URL
       publicId: String, // Cloudinary public_id
     },
