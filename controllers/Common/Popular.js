6c7b4c21e175f206d90b9bc22a06cd8520ab53f6
Muhammad khattab <muhammadkh.15.9.2000@gmail.com>
2025-03-10 03:30:46 +0300

    Exclude popular products that refer to non-existent products.

--- controllers/Common/Popular.js
+++ controllers/Common/Popular.js
@@ -5,8 +5,13 @@ exports.getPopulars = async (req, res) => {
   try {
     const populars = await Popular.find()
       .sort({ orderNumber: 1 })
-      .populate({path:'product', select:'_id title price images'})
-    res.status(200).json(populars);
+      .populate({
+        path: 'product',
+        select: '_id title price images',
+        match: { _id: { $ne: null } },
+      });
+    const filteredPopulars = populars.filter((p) => p.product);
+    res.status(200).json({ populars: filteredPopulars });
   } catch (error) {
     console.error('Error fetching populars:', error);
     res.status(500).json({ error: 'Internal server error' });
